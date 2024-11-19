0d58d0d82d6f8c2024ee4f16304ed873e84d90e6
Michaela <mi-and@outlook.com>
2024-11-19 13:56:07 +0100

    Replace userform to component

--- src/page/StartPage.vue
+++ src/page/StartPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import Header from "../components/Header.vue";
+import UserForm from "../components/UserForm.vue";
 
 const isOpen = ref(false);
 
@@ -42,39 +43,7 @@ const toggleDiv = () => {
   </div>
   <div v-if="isOpen" class="flex px-2 justify-center md:py-2">
     <div class="flex justify-center items-center max-w-[700px] w-full py-2">
-      <form
-        class="flex flex-col bg-white px-2 py-4 border border-gray-200 rounded-md shadow-lg gap-2 max-w-[550px] w-full"
-      >
-        <p>Registrera användare</p>
-
-        <input
-          type="text"
-          placeholder="Förnamn"
-          class="w-full px-2 py-2 border border-gray-300 rounded-md"
-          required
-        />
-
-        <input
-          type="text"
-          placeholder="Användarnamn"
-          class="w-full px-2 py-2 border border-gray-300 rounded-md"
-          required
-        />
-
-        <input
-          type="password"
-          placeholder="Lösenord"
-          class="w-full px-2 py-2 border border-gray-300 rounded-md"
-          required
-        />
-
-        <button
-          type="submit"
-          class="max-w-52 bg-black text-white py-2 px-4 rounded-md hover:bg-slate-500"
-        >
-          Skicka
-        </button>
-      </form>
+      <UserForm />
     </div>
   </div>
 </template>
