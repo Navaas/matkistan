0fefb66c916ed55fe676f0a5906ab1fa9a55dd93
Michaela <mi-and@outlook.com>
2024-11-19 14:04:14 +0100

    Can console.log new user

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,14 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const firstName = ref("");
+const userName = ref("");
+const password = ref("");
+
+const registerNewUser = () => {
+  console.log(firstName.value, userName.value, password.value);
+
+  (firstName.value = ""), (userName.value = ""), (password.value = "");
+};
+</script>
 
 <template>
   <form
     class="flex flex-col bg-white px-2 py-4 border border-gray-200 rounded-md shadow-lg gap-2 max-w-[550px] w-full"
+    @submit.prevent="registerNewUser"
   >
     <p>Registrera användare</p>
 
     <input
       type="text"
       placeholder="Förnamn"
+      v-model="firstName"
       class="w-full px-2 py-2 border border-gray-300 rounded-md"
       required
     />
@@ -16,6 +30,7 @@
     <input
       type="text"
       placeholder="Användarnamn"
+      v-model="userName"
       class="w-full px-2 py-2 border border-gray-300 rounded-md"
       required
     />
@@ -23,6 +38,7 @@
     <input
       type="password"
       placeholder="Lösenord"
+      v-model="password"
       class="w-full px-2 py-2 border border-gray-300 rounded-md"
       required
     />
