ae41d0bf9b97159335aaedfe60a7d80ac390d76e
Michaela <mi-and@outlook.com>
2024-12-06 14:14:13 +0100

    Styling recipe

--- src/page/UsersCreatedRecipesPage.vue
+++ src/page/UsersCreatedRecipesPage.vue
@@ -25,11 +25,26 @@ onMounted(() => {
   <div class="p-4">
     <h1>Skapade Recept</h1>
     <div v-if="createdRecipes.length > 0">
-      <ul class="list-disc pl-6">
-        <li v-for="recipe in createdRecipes" :key="recipe._id">
+      <div
+        class="flex flex-col gap-4 w-full md:flex-row md:items-center md:justify-center md:flex-wrap"
+      >
+        <div
+          v-for="recipe in createdRecipes"
+          :key="recipe._id"
+          class="bg-green-100 w-full p-4 md:w-80"
+        >
+          <div v-for="image in recipe.imageUrl" class="pb-4">
+            <img
+              :src="image"
+              alt="Bild på recept"
+              class="w-full md:w-[500px] lg:w-[700px] h-auto rounded-sm"
+            />
+          </div>
           <span>{{ recipe.title }}</span>
-        </li>
-      </ul>
+          <p>Svårighetsgrad: {{ recipe.difficulty }}</p>
+          <p>Tillagningstid: {{ recipe.cookingTime }} min</p>
+        </div>
+      </div>
     </div>
     <p v-else>Du har inte skapat några recept ännu.</p>
   </div>
