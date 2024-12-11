cefc3dd4e2f349066dfcb2e44e0ed734067b88db
Michaela <mi-and@outlook.com>
2024-12-11 20:04:14 +0100

    Add z-index header and mobile styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,37 +10,32 @@ const checkLogin = () => {
   isLoggedIn.value = checkLoginStatus();
 };
 
-// const logout = () => {
-//   logoutUser();
-//   isLoggedIn.value = false;
-//   router.push("/");
-// };
-
 onMounted(checkLogin);
 </script>
 
 <template>
   <div
     v-if="isLoggedIn"
-    class="flex px-2 py-2 bg-black text-white cursor-pointer justify-between fixed bottom-0 left-0 w-full md:top-0 md:bottom-auto"
+    class="flex px-2 py-2 bg-black text-white cursor-pointer justify-between fixed bottom-0 left-0 w-full md:top-0 md:bottom-auto z-50"
   >
-    <span class="hidden md:block">Icon</span>
-
-    <router-link to="/">
-      <span class="material-icons"> home </span>
-    </router-link>
-    <router-link to="/userCreatedRecipes">
-      <span class="material-icons">menu_book</span>
-    </router-link>
-    <router-link to="/createRecipe">
-      <span class="material-icons"> add </span>
-    </router-link>
-    <router-link to="/likedRecipes">
-      <span class="material-icons"> favorite </span>
-    </router-link>
-    <router-link to="/profil">
-      <span class="material-icons"> person </span>
-    </router-link>
+    <div><span class="hidden md:block">Icon</span></div>
+    <div class="flex gap-4">
+      <router-link to="/">
+        <span class="material-icons"> home </span>
+      </router-link>
+      <router-link to="/userCreatedRecipes">
+        <span class="material-icons">menu_book</span>
+      </router-link>
+      <router-link to="/createRecipe">
+        <span class="material-icons"> add </span>
+      </router-link>
+      <router-link to="/likedRecipes">
+        <span class="material-icons"> favorite </span>
+      </router-link>
+      <router-link to="/profil">
+        <span class="material-icons"> person </span>
+      </router-link>
+    </div>
   </div>
 
   <div
@@ -49,22 +44,22 @@ onMounted(checkLogin);
   >
     <button @click="router.push('/login')">Logga in</button>
   </div>
+
   <div
     v-if="isLoggedIn"
-    class="flex px-2 py-2 bg-black text-white cursor-pointer fixed bottom-0 left-0 w-full md:top-0 md:bottom-auto"
+    class="flex px-2 py-2 bg-black text-white cursor-pointer justify-between fixed bottom-0 left-0 w-full md:top-0 md:bottom-auto z-50"
   >
-    <span class="hidden md:block md:flex-shrink-0">
-      <span>Icon</span>
-    </span>
+    <!-- Icon placeholder for larger screens (hidden on mobile) -->
+    <div><span class="hidden md:block">Icon</span></div>
 
-    <div class="flex justify-around w-full md:justify-end md:gap-4 md:pr-4">
+    <!-- Flex container for icons -->
+    <div class="flex w-full justify-around md:justify-end gap-4 md:pr-4">
       <router-link to="/">
         <span class="material-icons">home</span>
       </router-link>
       <router-link to="/userCreatedRecipes">
         <span class="material-icons">menu_book</span>
       </router-link>
-
       <router-link to="/createRecipe">
         <span class="material-icons">add</span>
       </router-link>
@@ -76,7 +71,6 @@ onMounted(checkLogin);
       </router-link>
     </div>
   </div>
-
   <div
     v-else
     class="flex justify-between px-2 py-2 bg-black text-white cursor-pointer fixed top-0 left-0 w-full"
