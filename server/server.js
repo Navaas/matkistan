6dd0dd64edca6f4a41d60dfbce0fb33807b93ce9
Michaela <mi-and@outlook.com>
2024-12-17 10:23:53 +0100

    vercel

--- server/server.js
+++ server/server.js
@@ -10,12 +10,13 @@ async function main() {
     await mongoose.connect(process.env.DB_URI);
     console.log("Successfully connected to MongoDB!");
 
-    app.listen(3000, () => {
-      console.log("Server is running at http://localhost:3000");
-    });
+    // app.listen(3000, () => {
+    //   console.log("Server is running at http://localhost:3000");
+    // });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
 }
 
 main().catch((error) => console.error(error));
+export default app;
